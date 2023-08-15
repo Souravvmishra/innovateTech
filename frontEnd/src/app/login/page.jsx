9ea41ec86e59abb2b530d50cf4a276f9c9632d12
SouravvMishra <souravvmishra@gmail.com>
2023-08-15 15:21:02 +0530

    Auth Added Successfully

---
+++ frontEnd/src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+const LoginPage = () => {
+  return (
+    <div>
+      This is login page
+    </div>
+  )
+}
+
+export default LoginPage
