221ff09c45ad124292b97441f83c0ddea0e97ad8
bit-web24 <bitweb24@gmail.com>
2023-08-16 23:31:48 +0530

    client req

--- blockchain/client/main.js
+++ blockchain/client/main.js
@@ -1,10 +1,57 @@
-const { Connection, PublicKey, Account, Transaction, TransactionInstruction } = require('@solana/web3.js');
+const { Connection, PublicKey, Keypair, SystemProgram, Transaction, TransactionInstruction, LAMPORTS_PER_SOL } = require('@solana/web3.js');
 
 // Solana network URL
-const networkUrl = 'https://api.devnet.solana.com';
+//const networkUrl = 'https://api.devnet.solana.com';
+const networkUrl = 'http://127.0.0.1:8899';
 
-async function submitProject() {
+async function createPdaAccount(seedInfo) {
     const connection = new Connection(networkUrl, 'confirmed');
+
+    // Replace with your program ID and account keys
+    const programId = new PublicKey('5kBNQqvNeJDYurxktaTVSB6ZjiAGuBwbR1HzEWKcKP5');
+    // const initializerAccount = Keypair.generate(); // A new account to be used as the initializer
+
+
+    let secretKey = Uint8Array.from([211, 24, 17, 4, 217, 66, 191, 237, 29, 35, 5, 188, 88, 143, 186, 86, 7, 103, 93, 241, 23, 5, 75, 89, 209, 131, 130, 126, 133, 21, 161, 101, 170, 31, 172, 32, 225, 190, 109, 115, 19, 213, 50, 169, 182, 71, 105, 190, 164, 50, 75, 187, 133, 201, 60, 200, 22, 225, 71, 168, 240, 255, 56, 108]);
+
+    let initializerAccount = Keypair.fromSecretKey(secretKey);
+
+    const pdaSeed = seedInfo + '-pda-seed';
+    const pdaAccount = await PublicKey.createWithSeed(initializerAccount.publicKey, pdaSeed, programId);
+
+    // Create a new PDA account
+    const lamports = 1000000; // Amount of lamports to allocate for the PDA account
+    const rent = await connection.getMinimumBalanceForRentExemption(100); // Get rent exemption threshold
+    const transaction = new Transaction().add(
+        SystemProgram.createAccountWithSeed({
+            fromPubkey: initializerAccount.publicKey,
+            basePubkey: initializerAccount.publicKey,
+            seed: pdaSeed,
+            newAccountPubkey: pdaAccount,
+            lamports: lamports + rent, // Include rent in the allocated lamports
+            space: 100, // Replace with the required space
+            programId,
+        })
+    );
+
+    // Sign and send the transaction
+    const signature = await connection.sendTransaction(transaction, [initializerAccount], {
+        skipPreflight: false, // Set to true if you encounter preflight errors
+        preflightCommitment: 'confirmed', // Adjust as needed
+    });
+
+    console.log('PDA Account Created:', pdaAccount.toBase58(), "signature: ", signature);
+    return pdaAccount;
+}
+
+async function createProject() {
+    const connection = new Connection(networkUrl, 'confirmed');
+
+    // Replace with your program ID and account keys
+    const programId = new PublicKey('5kBNQqvNeJDYurxktaTVSB6ZjiAGuBwbR1HzEWKcKP5');
+    let secretKey = Uint8Array.from([211, 24, 17, 4, 217, 66, 191, 237, 29, 35, 5, 188, 88, 143, 186, 86, 7, 103, 93, 241, 23, 5, 75, 89, 209, 131, 130, 126, 133, 21, 161, 101, 170, 31, 172, 32, 225, 190, 109, 115, 19, 213, 50, 169, 182, 71, 105, 190, 164, 50, 75, 187, 133, 201, 60, 200, 22, 225, 71, 168, 240, 255, 56, 108]);
+
+    let initializerAccount = Keypair.fromSecretKey(secretKey);
 
     // Replace with your project data
     const projectData = {
@@ -15,10 +62,7 @@ async function submitProject() {
         project_creators: ['Creator 1', 'Creator 2'],
     };
 
-    // Replace with your program ID and account keys
-    const programId = new PublicKey('');
-    const initializerAccount = new Account();
-    const pdaAccount = new Account();    
+    const pdaAccount = await createPdaAccount(projectData.id.toString());
 
     const instructionData = {
         submitProject: {
@@ -30,21 +74,26 @@ async function submitProject() {
         },
     };
 
+    // Create a TransactionInstruction to submit the project data
     const instruction = new TransactionInstruction({
         programId,
         keys: [
             { pubkey: initializerAccount.publicKey, isSigner: true, isWritable: false },
-            { pubkey: pdaAccount.publicKey, isSigner: false, isWritable: true },
+            { pubkey: pdaAccount, isSigner: false, isWritable: true },
         ],
-        data: Buffer.from(JSON.stringify(instructionData)),
+        data: Buffer.from([0, ...Buffer.from(JSON.stringify(instructionData))]), // First byte is operation type
     });
 
+    // Create a Transaction containing the instruction
     const transaction = new Transaction().add(instruction);
 
     // Sign and send the transaction
-    const signature = await connection.sendTransaction(transaction, [initializerAccount, pdaAccount]);
+    const signature = await connection.sendTransaction(transaction, [initializerAccount], {
+        skipPreflight: false, // Set to true if you encounter preflight errors
+        preflightCommitment: 'confirmed', // Adjust as needed
+    });
 
     console.log('Transaction Signature:', signature);
 }
 
-submitProject().catch(error => console.error(error));
+createProject().catch(error => console.error(error));
--- blockchain/src/structs.rs
+++ blockchain/src/structs.rs
@@ -18,8 +18,45 @@ pub enum Instruction {
 
 impl Instruction {
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        BorshDeserialize::try_from_slice(input).map_err(|_| ProgramError::InvalidInstructionData)
+        let operation_type = input[0];
+        match operation_type {
+            0 => {
+                let submit_data = SubmitProjectData::try_from_slice(&input[1..])?;
+                Ok(Instruction::SubmitProject {
+                    id: submit_data.id,
+                    title: submit_data.title,
+                    description: submit_data.description,
+                    prototype_data: submit_data.prototype_data,
+                    project_creators: submit_data.project_creators,
+                })
+            }
+            1 => {
+                let fund_data = FundProjectData::try_from_slice(&input[1..])?;
+                Ok(Instruction::FundProject {
+                    project_id: fund_data.project_id,
+                    amount: fund_data.amount,
+                })
+            }
+            _ => {
+                return Err(ProgramError::InvalidInstructionData);
+            }
+        }
     }
+}
+
+#[derive(Debug, BorshSerialize, BorshDeserialize, PartialEq)]
+pub struct SubmitProjectData {
+    pub id: u8,
+    pub title: String,
+    pub description: String,
+    pub prototype_data: String,
+    pub project_creators: Vec<String>,
+}
+
+#[derive(Debug, BorshSerialize, BorshDeserialize, PartialEq)]
+pub struct FundProjectData {
+    pub project_id: u8,
+    pub amount: u64,
 }
 
 #[derive(Debug, BorshSerialize, BorshDeserialize, PartialEq)]
